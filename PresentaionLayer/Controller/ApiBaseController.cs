22dcf93ef8f38c745fe6d630763d9dc69581eb99
Yuossef-ezzat <yuossefezzatmostafa@gmail.com>
2026-03-14 14:55:50 +0200

    .

---
+++ PresentaionLayer/Controller/ApiBaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentaionLayer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiBaseController : ControllerBase
+    {
+    }
+}
---
+++ PresentaionLayer/Controller/PaymobController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PresentaionLayer.Controller;
+
+namespace EventManagament.Controllers
+{
+
+    public class PaymobController : ApiBaseController
+    {
+        //[HttpPost("callback")]
+        //[AllowAnonymous]
+        //public async Task<IActionResult> PaymobCallback([FromBody] PaymobCallback payload)
+        //{
+        //    ResponseModel<object> result = null!;
+        //    try
+        //    {
+        //        Logger.LogInformation("Received Paymob callback: {@Payload}", payload);
+
+        //        var hmacHeader = Request.Query["hmac"].FirstOrDefault();
+
+        //        result = await walletService.PaymobCallback(payload, hmacHeader);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Logger.LogError(ex, "Error logging Paymob callback");
+        //    }
+        //    return result.IsSuccess ? Ok(result) : BadRequest(result.Message);
+        //}
+    }
+}
